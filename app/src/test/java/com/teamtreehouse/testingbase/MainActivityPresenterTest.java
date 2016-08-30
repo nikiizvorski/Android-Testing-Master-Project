a22ce96f16ea07597d7398791e7e8e4a50c7c4db
NIKI <nikiizvorski@gmail.com>
2016-08-30 09:44:28 +0100

    Adding Mockito with Mock and 3AAA Approach!

--- app/src/test/java/com/teamtreehouse/testingbase/MainActivityPresenterTest.java
+++ app/src/test/java/com/teamtreehouse/testingbase/MainActivityPresenterTest.java
@@ -1,59 +1,67 @@
 package com.teamtreehouse.testingbase;
 
+import android.graphics.Color;
+import android.widget.TextView;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.junit.Assert.*;
 
 /**
  * Created by NIKI on 8/30/2016.
  */
+@RunWith(MockitoJUnitRunner.class)
 public class MainActivityPresenterTest {
 
     MainActivityPresenter presenter;
+
+    @Mock
     MainActivityView view;
-
-    class MockedView implements MainActivityView {
-
-
-        @Override
-        public void changeTextViewText(String text) {
-
-        }
-
-        @Override
-        public void changeBackgroundColor(int color) {
-
-        }
-
-        @Override
-        public void launchOtherActivity(Class activity) {
-
-        }
-    }
 
     @Before
     public void setUp() throws Exception {
-            view = new MockedView();
             presenter = new MainActivityPresenter(view);
     }
 
     @Test
-    public void testEditTextUpdated() throws Exception {
+    public void editTextUpdated() throws Exception {
             //Arrange
+            String givenString = "test123";
 
             //Act
+            presenter.editTextUpdated(givenString);
 
             //Assert
+        Mockito.verify(view).changeTextViewText(givenString);
     }
 
     @Test
-    public void testColorSelected() throws Exception {
+    public void colorSelected() throws Exception {
+            // Arrange
+            int index = 2;
+            int givingColor = Color.GREEN;
 
+            // Act
+            presenter.colorSelected(index);
+
+            // Assert
+            Mockito.verify(view).changeBackgroundColor(givingColor);
     }
 
     @Test
-    public void testLaunchOtherActivityButtonClicked() throws Exception {
+    public void launchOtherActivityButtonClicked() throws Exception {
+        // Arrange
+        Class clazz = OtherActivity.class;
 
+        // Act
+        presenter.launchOtherActivityButtonClicked(clazz);
+
+        // Assert
+        Mockito.verify(view).launchOtherActivity(clazz);
     }
 }
